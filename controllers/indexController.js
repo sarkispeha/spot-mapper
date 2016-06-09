0d788b4e7fa7d08b3c98b9921c0eda7f5bfc3cb8
Sarkis Peha <sarkispeha@gmail.com>
2016-06-09 14:16:20 -0600

    log a bit

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,6 +3,7 @@ var Point = require('../models/points.js');
 
 var indexController = {
 	allPoints: function(req, res){
+		console.log('getting points');
 		Point.find({}, (err, results)=>{
 			console.log('point find err ', err);
 			res.render('index', 
